32ea120c5d2bd7b440cf25cf18a6429fdf4288b8
satip1 <satip1@yandex.ru>
2023-07-14 04:15:14 +0300

    smallchips

---
+++ html/left move menu/index.js
@@ -0,0 +1,4 @@
+document.querySelector('.btnMove').addEventListener('click', (evt) => {
+    document.querySelector('.leftmenu').classList.toggle('leftmenu_active');
+    document.querySelector('.content').classList.toggle('content_active');
+})
